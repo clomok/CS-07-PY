189e03ae8ccbf0587bc8a216ebc59ec1dde734f0
clomok <96883102+clomok@users.noreply.github.com>
2022-02-04 14:16:59 -0800

    Add files via upload

--- 04 - File System and Error Handling/04-L8.py
+++ 04 - File System and Error Handling/04-L8.py
@@ -7,6 +7,8 @@ import os
 
 try:
     os.mkdir(r"C:\Users\clomo\Downloads\Cars")
+except:
+    print("This directory already exists")
 with open(r"C:\Users\clomo\Downloads\Cars\Mustang.txt", "a+") as file:
     pass
 path = input("Enter directory path: ")
@@ -14,5 +16,5 @@ file_name = input("Enter file name: ")
 new_name = input("Enter a new name: ")
 os.system(r"copy {}\{} {}\{}".format(path, file_name, path, new_name))
 
-except:
-    print("error occurred")
+#Cannot get it to complete the task it was designed for.
+#What is the purpose of asking the path?
