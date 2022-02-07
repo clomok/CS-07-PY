cadc8f3cd3c1f7c1f8714bd5d091c87f6a871ccd
clomok <96883102+clomok@users.noreply.github.com>
2022-02-06 16:26:05 -0800

    Delete 04 - File System and Error Handling directory

--- 04 - File System and Error Handling/04-L4.py
+++
@@ -1,8 +0,0 @@
-path = input("Enter a directory path for the text file: ")
-filename = input("Enter full filename: ")
-file = open(path + "\\" + filename, "r")
-for line in file:
-    print(line)
-
-file.close()
-input("\n Press 'Enter' to exit the program.")
