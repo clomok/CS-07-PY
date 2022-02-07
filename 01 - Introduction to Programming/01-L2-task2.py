5583af757b89db334feebbd205f099e6f1c43ba2
clomok <96883102+clomok@users.noreply.github.com>
2022-02-06 16:25:25 -0800

    Delete 01 - Introduction to Programming directory

--- 01 - Introduction to Programming/01-L2-task2.py
+++
@@ -1,7 +0,0 @@
-print("Hello World!")
-# This program will print to the console the string "Hello World!"
-
-print("I'm a Programmer.")
-"""
-Right click on the folder or python project directory and go to new > python file
-"""
